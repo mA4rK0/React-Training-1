0e9def38531ad128362a8b8143c240b1f87e2c73
mA4rK0 <onlymarko25@gmail.com>
2024-07-12 11:29:43 +0700

    learning about using Axios Web API in React.js

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import postsData from "../posts.json";
 import Article from "../components/Article";
 import Search from "../components/search";
@@ -6,15 +7,44 @@ import Search from "../components/search";
 function Homepage() {
   const [posts, setPosts] = useState(postsData);
   const [totalPosts, setTotalPosts] = useState(0);
+  const [externalPosts, setExternalPosts] = useState([]);
 
   const onSearchChange = (value) => {
-    console.log(value);
+    // console.log(value);
     const filteredPosts = postsData.filter((item) => {
       return item.title.includes(value);
     });
     setPosts(filteredPosts);
     setTotalPosts(filteredPosts.length);
   };
+
+  // useEffect(() => {
+  //   console.log("render");
+
+  //   return () => {
+  //     console.log("clean up");
+  //   };
+  // }, [posts]);
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      try {
+        const config = {
+          headers: {
+            Accept: "application/json",
+          },
+        };
+
+        const res = await axios.get("https://jsonplaceholder.typicode.com/todos", config);
+
+        setExternalPosts(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchApi();
+  }, []);
 
   return (
     <>
@@ -24,6 +54,11 @@ function Homepage() {
         // return <Article title={title} tags={tags} date={date} />; OR :
 
         return <Article {...{ title, tags, date, isNew }} key={index} />;
+      })}
+      <hr />
+      <h2>External Posts</h2>
+      {externalPosts.map((item, index) => {
+        return <div key={index}>- {item.title}</div>;
       })}
     </>
   );
